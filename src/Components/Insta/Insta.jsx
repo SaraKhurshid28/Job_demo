e65f079d5ca1af6bbab9b4731b8af60d4a060de1
Sara Khurshid <Sara.Khurshid@zweidevs.com>
2023-02-09 17:11:11 +0500

    Assests folder: add new images for insta page add new feature

--- src/Components/Insta/Insta.jsx
+++ src/Components/Insta/Insta.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row, Col } from "antd";
 import Images from "../../Constants/constants";
 import "./Insta.css";
 
@@ -12,6 +13,60 @@ const Insta = () => {
           </div>
           <div className="instaVerticalLine"></div>
           <div className="instaHeadingContainer">thetalhafakhar</div>
+        </div>
+        <div className="instaRowDiv">
+          <Row justify="center" className="instaRowContainer">
+            <Col span={8}>
+              <img
+                src={Images.SeptImage}
+                width={280}
+                height={280}
+                alt="Image Not Found"
+              />
+            </Col>
+            <Col span={8}>
+              <img
+                src={Images.OctImage2}
+                width={280}
+                height={280}
+                alt="Image Not Found"
+              />
+            </Col>
+            <Col span={8}>
+              <img
+                src={Images.OctImage}
+                width={280}
+                height={280}
+                alt="Image Not Found"
+              />
+            </Col>
+          </Row>
+          <Row justify="center" className="instaRowContainer">
+            <Col span={8}>
+              <img
+                src={Images.ITImage2}
+                width={280}
+                height={280}
+                alt="Image Not Found"
+              />
+            </Col>
+            <Col span={8}>
+              <img
+                src={Images.GrowImage}
+                width={280}
+                height={280}
+                alt="Image Not Found"
+              />
+            </Col>
+            <Col span={8}>
+              <img
+                src={Images.TFImage}
+                width={280}
+                height={280}
+                alt="Image Not Found"
+              />
+            </Col>
+          </Row>
         </div>
       </div>
     </>
--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -10,6 +10,12 @@ import blog2 from "../Assets/blog2.svg";
 import blog3 from "../Assets/blog3.svg";
 import blog4 from "../Assets/blog4.svg";
 import instaImage from "../Assets/instagram2_1 1.svg";
+import septPost from "../Assets/17septemberpost-011.svg";
+import octPost from "../Assets/20octobercarousel-061.svg";
+import octPost2 from "../Assets/25ocotobercarousel-011.svg";
+import growBusiness from "../Assets/growyourbusiness.svg";
+import tfPost from "../Assets/TFpost27september-011.svg";
+import tfpost2 from "../Assets/tfpost-021.svg";
 
 let Images = {
   Logo: whiteLogo,
@@ -24,6 +30,12 @@ let Images = {
   BlogImage3: blog3,
   BlogImage4: blog4,
   InstaImage: instaImage,
+  SeptImage: septPost,
+  OctImage: octPost,
+  OctImage2: octPost2,
+  GrowImage: growBusiness,
+  TFImage: tfPost,
+  ITImage2: tfpost2,
 };
 
 export default Images;
